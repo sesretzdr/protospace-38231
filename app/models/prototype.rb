746839d288338b21bcc763c2f182529435d81162
sesretzdr <3112117229@qq.com>
2022-10-21 00:29:01 +0900

    Update prototype.rb

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,8 +5,7 @@ class Prototype < ApplicationRecord
   validates :catch_copy, presence: true
   validates :concept, presence: true
   belongs_to :user
-  has_many :comments 
-  has_one_attached :image
+  has_many :comments, dependent: :destroy
 
 
   def was_attached?
