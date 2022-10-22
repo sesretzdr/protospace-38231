ac21ed6e3322beeea8dda676a088ce6f3135cbc3
sesretzdr <3112117229@qq.com>
2022-10-23 02:20:13 +0900

    Update prototype.rb

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,15 +1,13 @@
 class Prototype < ApplicationRecord
   has_one_attached :image
-  validates :content, presence: true, unless: :was_attached?
   validates :title, presence: true
   validates :catch_copy, presence: true
-  validates :concept, presence: true
+  validates :concept, presence: true  
+  validates :image, presence: true
   belongs_to :user
   has_many :comments 
-  has_one_attached :image
+ 
 
 
-  def was_attached?
-    self.image.attached?
-  end
+  
 end
